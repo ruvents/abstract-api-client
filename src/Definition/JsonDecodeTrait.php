24714517770bd1cb19490da624f4f780d715be9b
Valentin <udaltsov.valentin@gmail.com>
2017-11-21 15:12:01 +0300

    Simplified JsonDecodeTrait

--- src/Definition/JsonDecodeTrait.php
+++ src/Definition/JsonDecodeTrait.php
@@ -18,25 +18,12 @@ trait JsonDecodeTrait
      */
     public function decodeResponse(ResponseInterface $response, array $context)
     {
-        $decoded = $this->jsonDecode((string)$response->getBody());
+        $decoded = json_decode((string)$response->getBody());
 
         if (JSON_ERROR_NONE !== $code = json_last_error()) {
             throw new DecodeException($context, json_last_error_msg(), $code);
         }
 
         return $decoded;
-    }
-
-    /**
-     * @param string $string
-     * @param bool   $associative
-     * @param int    $depth
-     * @param int    $options
-     *
-     * @return mixed
-     */
-    protected function jsonDecode($string, $associative = true, $depth = 512, $options = 0)
-    {
-        return json_decode($string, $associative, $depth, $options);
     }
 }
